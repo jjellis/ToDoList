f16cf90c9917948fbd2b92dc58ebe97fedbe3549
jjellis <jjjr1234@aol.com>
2019-09-30 20:09:11 -0500

    added delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ITodo } from './interfaces/itodo';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
@@ -12,34 +13,45 @@ export class AppComponent {
   todoTitle: string;
   todoId: number;
 
+  constructor( private todoService: TodoService) {}
+
 
 ngOnInit() {
   this.todoTitle = '';  
   this.todoId= 1;
-  this.todoList = [
-    // example of how to make an item in todo list
-    {id: 1, isDoing: false, isEditing:false, title: 'Install Angular CLI', isDone: false },
-    
-  ];
+  this.todoList = this.todoService.todoList;
+  this.todoList.push({
+   
+    id: this.todoId, 
+    isDoing: false,
+     isEditing:false, 
+     title: 'Install Angular CLI', 
+     isDone: false 
+     //dateAdded: new Date()    
+    });
+  
+  
   
 }
 addTodo():void {
   this.todoId ++;
-  this.todoList.push({
+  const todo =({  
     id: this.todoId,
     title: this.todoTitle,
     isDone: false,
     isDoing: false,
     isEditing: false,
+    //dateAdded:new Date()
   });
-
-  
-  this.todoTitle = '';
-  this.todoId++;
+  this.todoService.add(todo);
+  this.todoTitle = '';  
 }
-deleteTodo(todo:any) {
-  const index = this.todoList.findIndex(todoItem => todoItem === todo);
-  this.todoList.splice(index, 1);
+toggleDone(todo: ITodo){
+  todo.isDone = !todo.isDone;
+}
+
+deleteTodo(todo:ITodo) {
+  this.todoService.delete(todo);``
 }
 
 }
--- src/app/confirmation-modal/confirmation-modal.component.ts
+++ src/app/confirmation-modal/confirmation-modal.component.ts
@@ -1,13 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { ITodo } from '../interfaces/itodo';
+import { TodoService } from '../services/todo.service';
 
 @Component({
-  selector: 'app-confirmation-modal',
+  selector: 'confirmation-modal',
   templateUrl: './confirmation-modal.component.html',
   styleUrls: ['./confirmation-modal.component.scss']
 })
 export class ConfirmationModalComponent implements OnInit {
+  closeResult: string;
+ @Input() todo: ITodo;
+  constructor(
+    private modalService: NgbModal, 
+    private todoService : TodoService
+    ) {}
 
-  constructor() { }
+    deleteTodo(){
+      this.todoService.delete(this.todo);
+    }
+
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  
 
   ngOnInit() {
   }
